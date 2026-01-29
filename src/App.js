f806af8cb445b38a1e3811140a1677b46b8a0842
Olivier Bernard <OBernard2@slb.com>
2026-01-29 10:06:09 +0100

    boilerplate

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Welcome to React</h1>
+        <p>Static React Web App</p>
+      </header>
+      <main className="app-main">
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
